8329046d2c167fe27cee2c2024e7ce0f965c9b52
dhacommas <dhacommas@protonmail.com>
2023-05-13 23:25:22 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -142,10 +142,10 @@ fi
 if youtube_t=$(toml_get_table "YouTube"); then youtube_mode=$(toml_get "$youtube_t" "build-mode") || youtube_mode="apk"; else youtube_mode="module"; fi
 if music_arm_t=$(toml_get_table "Music-arm"); then music_arm_mode=$(toml_get "$music_arm_t" "build-mode") || music_arm_mode="apk"; else music_arm_mode="module"; fi
 if music_arm64_t=$(toml_get_table "Music-arm64"); then music_arm64_mode=$(toml_get "$music_arm64_t" "build-mode") || music_arm64_mode="apk"; else music_arm64_mode="module"; fi
-if [ "$youtube_mode" != module ] || [ "$music_arm_mode" != module ] || [ "$music_arm64_mode" != module ]; then
-	log "\nInstall [Vanced Microg](https://github.com/TeamVanced/VancedMicroG/releases) to be able to use non-root YouTube or Music"
-fi
-log "\n[revanced-magisk-module](https://github.com/j-hc/revanced-magisk-module)"
+#if [ "$youtube_mode" != module ] || [ "$music_arm_mode" != module ] || [ "$music_arm64_mode" != module ]; then
+#	log "\nInstall [Vanced Microg](https://github.com/TeamVanced/VancedMicroG/releases) to be able to use non-root YouTube or Music"
+#fi
+#log "\n[revanced-magisk-module](https://github.com/j-hc/revanced-magisk-module)"
 log "\n---\nChangelog:"
 log "$(cat $TEMP_DIR/*-rv/changelog.md)"
 
